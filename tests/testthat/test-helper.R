47782b4f7f007716c6415308b6a0280f6ff2c86d
JianingYao <jianingyao619@gmail.com>
2023-03-02 16:44:17 -0500

    use simpler tests

--- tests/testthat/test-helper.R
+++ tests/testthat/test-helper.R
@@ -1,6 +1,5 @@
-# source("./tests/testthat/helper.R")
 test_that("are_all_close returns TRUE when inputs are close", {
-  v <- rep(100, 100)
+  v <- c(10, 10)
   w <- v + 1e-8
   expect_true(are_all_close(v, w, abs_tol = 1e-6, rel_tol = 1e-6))
 }
@@ -8,16 +7,16 @@ test_that("are_all_close returns TRUE when inputs are close", {
 
 
 test_that("are_all_close returns FALSE when the relative error is above rel_tol", {
-  v <- rep(100, 100)
+  v <- c(0.1, 0.1)
   w <- v + 1e-2
-  expect_false(are_all_close(v, w, abs_tol = 1e-6, rel_tol = 1e-3))
+  expect_false(are_all_close(v, w, abs_tol = 1e-2, rel_tol = 1e-8))
 }
 )
 
 
 test_that("are_all_close returns FALSE when the absolute error is above abs_tol", {
-  v <- rep(100, 100)
-  w <- v + 1e-3
-  expect_false(are_all_close(v, w, abs_tol = 1e-3, rel_tol = 1e-6))
+  v <- c(0.1, 0.1)
+  w <- v + 1e-5
+  expect_false(are_all_close(v, w, abs_tol = 1e-8, rel_tol = 1e-2))
 }
 )
