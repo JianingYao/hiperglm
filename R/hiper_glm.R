3afbe18e0321a50168f6f92ca31f8e4f27b92ea4
JianingYao <jianingyao619@gmail.com>
2023-03-08 14:09:41 -0500

    update hiper_glm to include logit model

--- R/hiper_glm.R
+++ R/hiper_glm.R
@@ -5,15 +5,28 @@ hiper_glm <- function(design, outcome, model = "linear", method = "BFGS") {
     stop(sprintf("The model %s is not supported.", model))
   }
   if (model == "linear") {
-    hglm_out <- list()
-    class(hglm_out) <- "hglm"
     if (method == "pinv") {
-      hglm_out$coef <- lm_mle_pinv(design, outcome)
+      MLE <- lm_mle_pinv(design, outcome)
     } else if (method == "BFGS") {
-      hglm_out$coef <- lm_mle_BFGS(design, outcome)
+      MLE <- lm_mle_BFGS(design, outcome)
     } else {
-      stop("The function is yet to be implemented for other methods other than pseudo-inverse or BFGS.")
+      stop("The function is yet to be implemented for other methods other than
+           pseudo-inverse or BFGS for linear regression.")
     }
   }
+  if (model == "logit") {
+    hglm_out <- list()
+    class(hglm_out) <- "hglm"
+    if (method == "BFGS") {
+      MLE <- logit_mle_BFGS(design, outcome)
+    } else if (method == "newton") {
+      MLE <- logit_mle_newton(design, outcome, tol = 1e-8)
+    } else {
+      stop("The function is yet to be implemented for other methods other than
+           Newton's method or BFGS for logistic regression.")
+    }
+  }
+  hglm_out <- list(coef = MLE)
+  class(hglm_out) <- "hglm"
   return(hglm_out)
 }
