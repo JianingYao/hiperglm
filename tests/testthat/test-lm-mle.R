c4df042773fe58efd8db3d54268906e8c100e14c
JianingYao <jianingyao619@gmail.com>
2023-03-08 08:07:33 -0500

    clean test-lm-mle.R

--- tests/testthat/test-lm-mle.R
+++ tests/testthat/test-lm-mle.R
@@ -1,4 +1,4 @@
-test_that("return TRUE if MLE via pseudo-inverse is close to truth", {
+test_that("return TRUE if MLE via pseudo-inverse is close to truth in linear regression", {
   set.seed(1234)
   design <- as.matrix(cbind(rep(1, 5), rnorm(5, 0, 1), rnorm(5, 10, 2)))
   truth <- as.matrix(c(5, -4, 2.5))
@@ -8,7 +8,7 @@ test_that("return TRUE if MLE via pseudo-inverse is close to truth", {
   ))
 })
 
-test_that("return TRUE if MLE via BFSG is close to truth", {
+test_that("return TRUE if MLE via BFSG is close to truth in linear regression", {
   set.seed(1234)
   design <- as.matrix(cbind(rep(1, 5), rnorm(5, 0, 1), rnorm(5, 10, 2)))
   truth <- as.matrix(c(5, -4, 2.5))
@@ -19,7 +19,7 @@ test_that("return TRUE if MLE via BFSG is close to truth", {
   ))
 })
 
-test_that("return TRUE if analytical and numerical gradient match", {
+test_that("return TRUE if analytical and numerical gradient match in linear regression", {
   set.seed(1234)
   design <- as.matrix(cbind(rep(1, 5), rnorm(5, 0, 1), rnorm(5, 10, 2)))
   truth <- as.matrix(c(5, -4, 2.5))
@@ -38,7 +38,7 @@ test_that("return TRUE if analytical and numerical gradient match", {
   expect_true(grads_are_close)
 })
 
-test_that("return TRUE if MLE via pseudo-inverse and BFGS match", {
+test_that("return TRUE if MLE via pseudo-inverse and BFGS match in linear regression", {
   set.seed(1234)
   design <- as.matrix(cbind(rep(1, 5), rnorm(5, 0, 1), rnorm(5, 10, 2)))
   truth <- as.matrix(c(5, -4, 2.5))
@@ -49,16 +49,3 @@ test_that("return TRUE if MLE via pseudo-inverse and BFGS match", {
     abs_tol = 1e-3, rel_tol = 1e-3
   ))
 })
-
-# test_that("newton and bfgs outputs coincide on logit model", {
-#   n_obs <- 32; n_pred <- 4
-#   data <- simulate_data(n_obs, n_pred, model = 'logit', seed = 1918)
-#   design <- data$design; outcome <- data$outcome
-#   via_newton_out <- hiper_glm(design, outcome, model = 'logit')
-#   via_bfgs_out <- hiper_glm(
-#     design, outcome, model = 'logit', option = list(mle_solver = 'BFGS')
-#   )
-#   expect_true(are_all_close(
-#     coef(via_newton_out), coef(via_bfgs_out), abs_tol = 1e-2, rel_tol = 1e-2
-#   ))
-# })
